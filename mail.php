0a9147aa57f75bbf1085713e558042d591d0b55e
Ricardo Rohrs <rerohrs@inf.ufsm.br>
2025-01-13 16:38:53 -0300

    chore: add rm email to form

--- mail.php
+++ mail.php
@@ -1,52 +1,37 @@
 <?php
-    // Only process POST requests.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
         $name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $phone = trim($_POST["phone"]);
         $message = trim($_POST["message"]);
 
-        // Check that data was sent to the mailer.
         if (empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            echo "Por favor complete o formulário e tente novamente.";
             exit;
         }
 
-        // Set the recipient email address.
-        // Note:  Update this to your desired email address.
-        $recipient = "wpsakib@gmail.com";
+        $recipient = "rh@rmautomotive.com.br";
 
-        // Set the email subject.
-        $subject = "New Contact Form Submission";
+        $subject = "Novo Envio do Formilário de Contato";
 
-        // Build the email content.
-        $email_content = "Name: $name  \r\n\n";
+        $email_content = "Nome: $name  \r\n\n";
         $email_content .= "Email: $email \r\n\n";
-        $email_content .= "Phone: $phone \r\n\n";
-        $email_content .= "Message: $message \r\n\n";
+        $email_content .= "Celular: $phone \r\n\n";
+        $email_content .= "Mensagem: $message \r\n\n";
 
-        // Build the email headers.
         $email_headers = "From: $name <$email>";
 
-        // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Obrigado! Sua mensagem foi enviada.";
         } else {
-            // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Oops! Algo deu errado e não conseguimos enviar a sua mensagem.";
         }
-
     } else {
-        // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Ocorreu um erro no envio da mensagem, por favor tente novamente.";
     }
-
 ?>
